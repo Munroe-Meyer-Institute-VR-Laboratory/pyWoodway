aea9ff5693ad0e25d139b43e6d7fe85536b587c8
Walker Arce <wsarcera@gmail.com>
2022-06-05 17:24:54 -0500

    Fixed citation, updated setup for pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup(name='pyWoodway',
       version='0.2.1',
       url='https://github.com/Munroe-Meyer-Institute-VR-Laboratory/pyWoodway',
       author='Walker Arce',
       author_email='walker.arce@unmc.edu',
       description='Communicate with your Woodway treadmill in your Python scripts.',
+      keywords='Woodway treadmill biomechanics',
+      long_description=readme,
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.8'
+      ],
+      long_description_content_type="text/markdown",
       # packages=find_packages(),
-      long_description=open('README.md').read(),
       zip_safe=False,
       license='MIT',
+      include_package_data=True,
       packages=['pywoodway'],
       install_requires=[
             'pyserial'
